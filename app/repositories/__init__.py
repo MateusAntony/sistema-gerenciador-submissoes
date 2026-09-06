95ea635a0858f85daa90191f08a1d813c1f10a72
José Victor <josevictoroliveira160@gmail.com>
2026-09-06 11:17:47 -0300

    Update __init__.py

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,17 +1,3 @@
-from flask import Flask
-from app.config import Config
-from app.extensions import db, bcrypt
-from app.controllers.auth_controller import auth_bp
+from app.repositories.user_repository import UserRepository
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    # Inicialização de Extensões
-    db.init_app(app)
-    bcrypt.init_app(app)
-
-    # Registro de Blueprints
-    app.register_blueprint(auth_bp)
-
-    return app
+__all__ = ['UserRepository']
