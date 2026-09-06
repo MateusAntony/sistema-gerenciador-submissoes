ce527ed31523259506e0529abdc281b3a0903acf
José Victor <josevictoroliveira160@gmail.com>
2026-09-06 11:44:27 -0300

    Update user_repository.py

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -4,19 +4,20 @@ from app.extensions import db
 class UserRepository:
     @staticmethod
     def get_by_email(email: str) -> Usuario:
-        return Usuario.query.filter_eq(email=email).first()
+        return Usuario.query.filter_by(email=email).first()
 
     @staticmethod
     def get_by_id(user_id: int) -> Usuario:
         return Usuario.query.get(user_id)
 
     @staticmethod
-    def create(nome: str, email: str, senha_hash: str, instituicao: str = None) -> Usuario:
+    def create(nome: str, email: str, senha_hash: str, instituicao: str = None, pais: str = None) -> Usuario:
         user = Usuario(
             nome=nome,
             email=email,
             senha_hash=senha_hash,
-            instituicao=instituicao
+            instituicao=instituicao,
+            pais=pais
         )
         db.session.add(user)
         db.session.commit()
