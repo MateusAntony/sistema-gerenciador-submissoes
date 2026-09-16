6e4d7536bb7ed5fc1e12f099d5c58678cdc8665b
Weslei Santos <88506317+WesleiSantos13@users.noreply.github.com>
2026-09-16 12:46:44 -0300

    eventos_bp

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.config import Config
 from app.extensions import db, bcrypt
 from app.controllers.auth_controller import auth_bp
+from app.controllers.evento_controller import eventos_bp
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -13,5 +14,5 @@ def create_app(config_class=Config):
 
     # Registro de Blueprints
     app.register_blueprint(auth_bp)
-
+    app.register_blueprint(eventos_bp)
     return app
